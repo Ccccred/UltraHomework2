3d25b49f845eb595e2c21bfcd6d5e13e592a27aa
Ccccred <3565530270@qq.com>
2025-11-01 16:41:31 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -5,23 +5,16 @@
 int main(){
 int num=1,m=1;//n表示密码,m=1表示真，m=0表示假,这里一定要先默认m=1
 printf("请输入你的密码(50以内的整数)：");
-while(1){
-    scanf("%d",& num);
-    if(num>=50 || num<1){
-    printf("请输入50以内的正整数：");
-}
-    else{
-        break;
-    }
-}
+scanf("%d",& num);
 if(num==1){
     m=0;
 }
 else{
     for(int b=2 ; b<num ; b++)//相除的数从2开始，而不是从1开始
-    if(num % b==0)//一定要用取余
+    {if(num % b==0)//一定要用取余
     {m=0;
     break;}
+    }
 }
     
     if(m==0)
@@ -34,4 +27,3 @@ else{
     }
 
     return 0;
-}
