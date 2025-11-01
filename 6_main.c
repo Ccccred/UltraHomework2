dfa600d034f3171bc32d0f49cce09c1d2a9b293e
Ccccred <3565530270@qq.com>
2025-11-01 16:37:41 +0800

    Update 6_main.c

---
+++ 6_main.c
@@ -0,0 +1,23 @@
+/202510304103
+//3565530270@qq.com
+//刘栩池
+#include<stdio.h>
+int main(){
+int arr[5],i=0;//i表示数组中的第几个数
+int n;
+while(i<=4){
+    scanf("%d",&n);
+    while(n%2!=0){
+        scanf("%d",&n);
+    }
+    arr[i]=n;
+    i++;
+}
+for(i=0;i<=4;i++){
+    if(i==4)
+    printf("%d",arr[i]);
+    else
+    printf("%d ",arr[i]);
+}
+return 0;
+}
