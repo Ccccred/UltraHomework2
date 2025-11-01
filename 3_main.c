bdae524ce5b9829e1d4b2323a9a90848d5d5be20
Ccccred <3565530270@qq.com>
2025-11-01 16:50:07 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -4,7 +4,7 @@
 #include<stdio.h>
 int main(){
 int num=1,m=1,b=2;//n表示密码,这里要默认m=1,在后面的做判断的时候才是m==1
-printf("请输入你的密码(50以内的整数)：");
+//printf("请输入你的密码(50以内的整数)：");
 scanf("%d",&num);
 //1不是质数
 if(num==1){
@@ -13,7 +13,7 @@ if(num==1){
 else{
     while(b<num){
    if (num % b==0){
-    m=0;
+       m=0;
     break;//当其中有一个满足后就要退出循环，用break
    }
    b++;
