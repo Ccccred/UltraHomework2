da0a2c05d9704f1f7f21074cb86855163b5b22e0
Ccccred <3565530270@qq.com>
2025-11-07 15:31:41 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -3,25 +3,33 @@
 //刘栩池
 #include<stdio.h>
 int main(){
-int num=1,m=1,b=2;//num表示密码,这里要默认m=1,在后面的做判断的时候才是m==1
-printf("请输入你的密码(50以内的整数)：");
-scanf("%d",&num);
-//1不是质数
-if(num==1){
-    m=0;
+int i=0,j=0;
+int arr[10];
+for(int i=0;i<10;i++){
+    scanf("%d",&arr[i]);
 }
-else{
-    while(b<num){
-   if (num % b==0){
-    m=0;
-    break;//当其中有一个满足后就要退出循环，用break
-   }
-   b++;
- }
+
+for(i=0;i<10-1;i++){//有10个数字，但是只用进行9轮的比较,不要直接用9，而是用10-1去表示9
+    for(j=0;j<9-i;j++){//表示每轮要比较的次数
+        if(arr[j]>arr[j+1]){//要交换变量，就要引入中间变量
+            int a=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=a;
+            /* 与上面的是等价表示
+             int a=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=a; */
+        }
+    }
 }
-if(m==0)
-    {printf("密钥不安全，请重新输入");}
-    else
-    {printf("密钥安全，密码设置成功");}
+
+for(i=0;i<10;i++){
+    if(i==0){//表示第一个数字
+        printf("%d",arr[i]);
+    }
+    else{
+        printf(" %d",arr[i]);
+    }
+}
 return 0;
 }
