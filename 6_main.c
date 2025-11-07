acd707594237b3f5fefa2526b57ceffc641b1e9b
Ccccred <3565530270@qq.com>
2025-11-07 15:33:42 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,22 +2,29 @@
 //3565530270@qq.com
 //刘栩池
 #include<stdio.h>
-int main(){
-int arr[5],i=0;//i表示数组中的第几个数
-int n;
-while(i<=4){
-    scanf("%d",&n);
-    while(n%2!=0){
-        scanf("%d",&n);
+int calculateSum(int arr[],int length){
+    int sum=0;
+    for(int i=0;i<length;i++){
+        sum=sum+arr[i];
     }
-    arr[i]=n;
-    i++;
+    return sum;
 }
-for(i=0;i<=4;i++){
-    if(i==4)
-    printf("%d",arr[i]);
-    else
-    printf("%d ",arr[i]);
+
+int calculateProduct(int arr[],int length){
+    int product=1;
+    for(int i=0;i<length;i++){
+        product=product*arr[i];
+    }
+    return product;
 }
-return 0;
+
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }                           
+    int sum=calculateSum(arr,5);//这里不能写成arr[i],这个表示第几个元素，而这里要的是找到数组的首地址在哪里
+    int product=calculateProduct(arr,5);
+    printf("%d %d",sum,product); 
+    return 0;
 }
