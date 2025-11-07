641a1a549c50575473395f4bd0ffa849f5c04066
Ccccred <3565530270@qq.com>
2025-11-07 15:34:15 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,24 +2,46 @@
 //3565530270@qq.com
 //刘栩池
 #include<stdio.h>
+void reverse(int arr[],int length){
+    int start=0;
+    int end=length -1;
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;    
+    }
+}
+
 int main(){
-int arr[5];//学号一共是5个数组
-int i=0;//编号从零开始
-for(i=0;i<5;i++){
-    scanf("%d",&arr[i]);
-}
-//一定要用i--
-for(i=4;i>=0;i--){
-    arr[i]=arr[i-1];
-}
-//arr[0]=0;
-for(i=0;i<5;i++){
-    if(i==0){
-        printf("0");
+    int arr[5];
+    //int length=5;
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
     }
-    else{
-        printf(" %d",arr[i]);
+    reverse(arr,5);
+    for(int i=0;i<5;i++){
+        if(i==0){
+            printf("%d",arr[i]);
+        }
+        else{
+            printf(" %d",arr[i]);
+        }
     }
+    return 0;
 }
-return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
