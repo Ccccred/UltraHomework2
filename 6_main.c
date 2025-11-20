93d66c0f4471c64bd0b0b82f2fd67357d2aa9d97
Ccccred <3565530270@qq.com>
2025-11-20 09:37:03 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,29 +2,20 @@
 //3565530270@qq.com
 //刘栩池
 #include<stdio.h>
-int calculateSum(int arr[],int length){
-    int sum=0;
-    for(int i=0;i<length;i++){
-        sum=sum+arr[i];
-    }
-    return sum;
-}
-
-int calculateProduct(int arr[],int length){
-    int product=1;
-    for(int i=0;i<length;i++){
-        product=product*arr[i];
-    }
-    return product;
-}
+#include<stdlib.h>//表示申请一个储存空间
 
 int main(){
-    int arr[5];
-    for(int i=0;i<5;i++){
-        scanf("%d",&arr[i]);
-    }                           
-    int sum=calculateSum(arr,5);//这里不能写成arr[i],这个表示第几个元素，而这里要的是找到数组的首地址在哪里
-    int product=calculateProduct(arr,5);
-    printf("%d %d",sum,product); 
+    int *p;
+    int i;
+    p=(int*)malloc(5*sizeof(int));
+    for(i=0;i<5;i++){
+        scanf("%d",&p[i]);
+    }
+    for(i=0;i<5;i++){
+        printf("%d ",p[i]);
+    }
+
+    free(p);//表示释放内存
+
     return 0;
 }
