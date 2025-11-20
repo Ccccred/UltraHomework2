65478fb1247ce75af07f7e01e12e978477ac93bb
Ccccred <3565530270@qq.com>
2025-11-20 09:37:30 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,34 +2,29 @@
 //3565530270@qq.com
 //刘栩池
 #include<stdio.h>
-void reverse(int arr[],int length){
-    int start=0;
-    int end=length -1;
-    while(start<end){
-        int temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
-        start++;
-        end--;    
-    }
-}
-
 int main(){
-    int arr[5];
-    //int length=5;
-    for(int i=0;i<5;i++){
-        scanf("%d",&arr[i]);
+    int arr[10];
+    int *p=arr;//int *p;p=arr就是正确的，但是int*p;*p=arr就是错误的
+    int i,j,temp;
+
+    for(i=0;i<10;i++){
+        scanf("%d",p+i);//表示的是第i个元素，是在arr里面的第几个元素
     }
-    reverse(arr,5);
-    for(int i=0;i<5;i++){
-        if(i==0){
-            printf("%d",arr[i]);
-        }
-        else{
-            printf(" %d",arr[i]);
+
+    for(i=0;i<10-1;i++){//10-1 表示10个数但是只进行9轮的比较
+        for(j=0;j<10-1;j++){
+            if(*(p+j)>*(p+j+1)){
+                temp=*(p+j);
+                *(p+j)=*(p+j+1);
+                *(p+j+1)=temp;
+            }
         }
     }
-    return 0;
+
+     for(i=0;i<10;i++){
+        printf("%d ",*(p+i));
+    }
+     return 0;
 }
 
 
