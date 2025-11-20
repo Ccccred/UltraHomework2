0fce3e27ba60b14b14f17aabf94c05d4173ef854
Ccccred <3565530270@qq.com>
2025-11-20 09:36:06 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,19 +2,21 @@
 //3565530270@qq.com
 //刘栩池
 #include<stdio.h>
-int power(int a,int b){
-    int result=1;
-    for(int i=0;i<b;i++){
-        result*=a;
+void increment(int *ptr,int size){
+    for(int i=0;i<size;i++){
+       (*ptr)++;//*ptr=*ptr+1,表示在指针变量对应的数字上加一，但是指针的位置不变。返回的是改变前的值
+        ptr++;
     }
-    return result;
 }
-int main(){
-    int sum=0;
-    for(int i=1;i<=5;i++){
-        sum+=power(i,2);
-    }
 
-    printf("%d\n",sum);
-return 0;
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+    increment(arr,5);
+    for(int i=0;i<5;i++){
+    printf("%d ",arr[i]);
+}
+    return 0;
 }
