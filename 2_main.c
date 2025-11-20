6e8dfea43d8035c97072397a9604d6ab114330d1
Ccccred <3565530270@qq.com>
2025-11-20 09:34:35 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -3,22 +3,27 @@
 //刘栩池
 #include<stdio.h>
 int main(){
-int arr[3][3];
-int i=0,j=0;
-for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-        scanf("%d",&arr[i][j]);
+    int arr[5];
+    int *p;
+    int i;//这里用整体变量，因为有好几个for循环中都要用到i
+    for(i=0;i<5;i++){
+        scanf("%d",&arr[i]);
     }
-}
+    p=arr;//这里不是&arr，而直接是arr
+          //arr表示的是地址，arr就是一个指针，*arr代表的是具体的某一个值
 
-for(i=0;i<3;i++){
-    for(j=0;j<3;j++){
-        if(j==0){//只能是列j，而不能是行i
-        printf("%d",arr[j][i]);}
-        else
-        {printf(" %d",arr[j][i]);}
+    for(i=0;i<5;i++){
+        *p=*p*2;
+        p++;//表示循环
     }
-    printf("\n");
+
+    for(i=0;i<5;i++){
+        if(i==0){
+             printf("%d",arr[i]);
+        }
+        else{
+        printf(" %d",arr[i]);
+    }
 }
-return 0;
+    return 0;
 }
